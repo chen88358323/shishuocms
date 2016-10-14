2fb39c1994503071d2b06522d2eb5f48d27103e3
chen88358323 <chch1020@163.com>
2016-10-15 01:32:51 +0800

    增加Service 测试用例

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,23 @@
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by cc on 16-10-15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class BaseTest {
+    protected final Logger logger = Logger.getLogger(this.getClass());
+
+    @Autowired
+    ApplicationContext ctx;
+
+    @Test
+    public void testEmployee(){
+    }
+}
